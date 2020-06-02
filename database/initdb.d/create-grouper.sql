8a1fd4f40d5e658b08c38f9dc53acf5867506781
Jj! <jj@unicon.net>
2020-06-01 21:44:02 -0500

    update configuration

--- database/initdb.d/create-grouper.sql
+++ database/initdb.d/create-grouper.sql
@@ -1,4 +1,3 @@
 CREATE USER grouper WITH PASSWORD 'grouper' CREATEDB;
-ALTER USER grouper WITH superuser;
+ALTER user grouper WITH superuser;
 CREATE DATABASE grouper;
-GRANT ALL PRIVILEGES ON DATABASE grouper TO grouper;
